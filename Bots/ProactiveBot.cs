051bad0fee7c92ace092a641e4bbb6b51e5a74e7
Meenakshi Balekar <mebaleka@microsoft.com>
2020-08-26 14:05:18 +0530

    Add project files.

---
+++ Bots/ProactiveBot.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class ProactiveBot : ActivityHandler
+    {
+
+        //private static Timer _timer;
+        // Message to send to users when the bot receives a Conversation Update event
+        private const string WelcomeMessage = "Welcome to the Proactive Bot sample.  Navigate to http://localhost:3978/api/notify to proactively message everyone who has previously messaged this bot.";
+
+        // Dependency injected dictionary for storing ConversationReference objects used in NotifyController to proactively message users
+        private readonly ConcurrentDictionary<string, ConversationReference> _conversationReferences;
+
+        public ProactiveBot(ConcurrentDictionary<string, ConversationReference> conversationReferences)
+        {
+            _conversationReferences = conversationReferences;
+        }
+
+        private void AddConversationReference(Activity activity)
+        {
+            var conversationReference = activity.GetConversationReference();
+            _conversationReferences.AddOrUpdate(conversationReference.User.Id, conversationReference, (key, newValue) => conversationReference);
+        }
+
+        protected override Task OnConversationUpdateActivityAsync(ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            AddConversationReference(turnContext.Activity as Activity);
+
+            return base.OnConversationUpdateActivityAsync(turnContext, cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                // Greet anyone that was not the target (recipient) of this message.
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(WelcomeMessage), cancellationToken);
+                }
+            }
+        }
+        private async Task BotCallback(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            // If you encounter permission-related errors when sending this message, see
+            // https://aka.ms/BotTrustServiceUrl
+            await turnContext.SendActivityAsync("proactive hello");
+        }
+        //private readonly IBotFrameworkHttpAdapter _adapter;
+        //private readonly string _appId = "c82a8f9c-0bd6-4c8a-8a37-af5c5bea4269";
+        public readonly ConversationReference conversationReferencesNew;
+        //private ConversationReference reference;
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+           
+        //timer details
+
+        // Save the conversationReference
+        ConversationReference conversationReference = turnContext.Activity.GetConversationReference();
+            _conversationReferences.AddOrUpdate(conversationReference.User.Id, conversationReference, (key, newValue) => conversationReference);
+            //var conversationReferencevalue = _conversationReferences.Values;
+            
+        // Reset the inactivity timer
+       
+            //closure of timer details
+
+            AddConversationReference(turnContext.Activity as Activity);
+
+            // Echo back what the user said
+            await turnContext.SendActivityAsync(MessageFactory.Text($"You sent '{turnContext.Activity.Text}'"), cancellationToken);
+
+            await Task.Delay(30000).ContinueWith(async (conversationReference) =>
+            {
+                //foreach (var reference in conversationReference1.Values)
+                //{
+                //await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, reference, BotCallback, default(CancellationToken));
+                //}
+                await turnContext.SendActivityAsync(MessageFactory.Text($"You der?"), cancellationToken);
+
+
+            }, cancellationToken);
+
+        }
+      
+    }
+}
